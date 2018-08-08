0871f43f385302d09934748df55bed8b3b723e84
chen wenhao <wenhao.chen@anlaa.com>
2018-08-08 11:43:09 +0800

    x

--- chen_blog/urls.py
+++ chen_blog/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('home/', views.home, name='home'),
     path('articles/<int:id>/', views.detail, name='detail'),
+    path('test/', views.test, name='test'),
     path('category/<int:id>/', views.search_category, name='category_menu'),
     path('tag/<str:tag>/', views.search_tag, name='search_tag'),
     path('archives/<str:year>/<str:month>', views.archives, name='archives'),  # 按月归档
