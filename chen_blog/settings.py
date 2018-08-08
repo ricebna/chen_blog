787b1beed229d80ec2aa8db0e70ef9eecb8fd597
chen wenhao <wenhao.chen@anlaa.com>
2018-08-08 09:32:43 +0800

    allow host

--- chen_blog/settings.py
+++ chen_blog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '*1%hfl_we7%me#&+=!q6t*f+22$ce(a@d&$yoj)kner1=y24uu'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['blog.zhihumm.com']
 
 
 # Application definition
